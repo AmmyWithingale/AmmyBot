8b9aba7b32134a0632c934181fe5c5a4f26d55f2
AmmyWithingale <65707030+AmmyWithingale@users.noreply.github.com>
2020-05-21 17:17:16 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,25 +4,25 @@ const client = new Discord.Client();
 
 client.on('ready',() => {
 	
-	   client.user.setPresence({game: {name: "with owo", type: 0}}); //Here you set the status of the bot like pixel that is playing in Japan or stuff like that
+	   client.user.setPresence({game: {name: "with Bam <33", type: 0}}); //Here you set the status of the bot like pixel that is playing in Japan or stuff like that
 });
 
-var prefix = "n!" //prefix, you can change it
+var prefix = "zr!" //prefix, you can change it
 
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'ping')) {
 	
-		 message.channel.send(`:ping_pong: Pong! \nTime taken: \`${Date.now() - message.createdTimestamp} ms\``);
+		 message.channel.send(`:ping_pong: Pingu Pomgus! \nTime taken: \`${Date.now() - message.createdTimestamp} ms\``);
 	}
 });  //Ping command owo pretty important for checking if bot works
 
 
 client.on('message', message => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'owo')) {  //Here you put the command like p!help or like that
+	if (message.content.startsWith(prefix + 'towerofgod')) {  //Here you put the command like p!help or like that
 	
-		 message.channel.send(`Simple text here owo`); //And here you put the answer of the bot owo
+		 message.channel.send(`To read the manwha of **Tower of God** click here: https://www.webtoons.com/en/fantasy/tower-of-god/list?title_no=95&page=48`); //And here you put the answer of the bot owo
 	}
 }); 
 
